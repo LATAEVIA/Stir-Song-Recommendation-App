e93a77d6251632302b503ec52d2196af84f57914
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-04 14:29:02 -0700

    add click listener and toast

--- app/src/main/java/com/lataeviaberry/stirsuggestions/MainActivity.java
+++ app/src/main/java/com/lataeviaberry/stirsuggestions/MainActivity.java
@@ -2,7 +2,9 @@ package com.lataeviaberry.stirsuggestions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindSongsButton;
@@ -12,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFindSongsButton = (Button) findViewById(R.id.findSongsButton)
+        mFindSongsButton = (Button) findViewById(R.id.findSongsButton);
+        mFindSongsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
