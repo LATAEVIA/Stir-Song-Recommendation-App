0139f054b36b8bd1398f2642184e2cae5aa665f4
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-04 21:17:24 -0700

    add font, and song activity intent and display of info

--- app/src/main/java/com/lataeviaberry/stirsuggestions/SongsActivity.java
+++ app/src/main/java/com/lataeviaberry/stirsuggestions/SongsActivity.java
@@ -1,13 +1,21 @@
 package com.lataeviaberry.stirsuggestions;
 
+import android.content.Intent;
+import android.sax.TextElementListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SongsActivity extends AppCompatActivity {
+    private TextView mSongTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs);
+        mSongTextView = (TextView) findViewById(R.id.songTextView);
+        Intent intent = getIntent();
+        String song = intent.getStringExtra("song");
+        mSongTextView.setText("Here are all the songs similar to " + song + ":");
     }
 }
