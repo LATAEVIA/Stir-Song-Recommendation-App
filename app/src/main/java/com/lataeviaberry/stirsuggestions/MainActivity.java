79fccb6cf6647d3c2cac25cef87c2e31a980edc1
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-04 21:55:15 -0700

    declare member var and define it onCreate()

--- app/src/main/java/com/lataeviaberry/stirsuggestions/MainActivity.java
+++ app/src/main/java/com/lataeviaberry/stirsuggestions/MainActivity.java
@@ -1,17 +1,17 @@
 package com.lataeviaberry.stirsuggestions;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindSongsButton;
     private EditText mSongEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mSongEditText = (EditText) findViewById(R.id.songEditText);
         mFindSongsButton = (Button) findViewById(R.id.findSongsButton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
 
         mFindSongsButton.setOnClickListener(new View.OnClickListener() {
             @Override
